ac1b0fa921bf0ae89d0bd949daba13d4b7258b50
MehvishAltafKhawaja <khawajamehvish13@outlook.com>
2026-04-01 20:06:15 +0530

    Added student details feature and updated home views

--- MyFirstProject/Controllers/HomeController.cs
+++ MyFirstProject/Controllers/HomeController.cs
@@ -20,16 +20,29 @@ namespace MyFirstProject.Controllers
         }
 
         public IActionResult AboutUs()
-        {  
+        {
+       
             return View();
         }
-           
 
+      
         public IActionResult Privacy()
         {
             return View();
         }
         
+        public IActionResult StudentDetail()
+        {
+            List<StudentDetails> std = new List<StudentDetails>()
+            {
+                new StudentDetails(){Name="Sahil",RollNo=01,Course= "Python",Fee= 50000,Address="Srinagar"},
+                new StudentDetails(){Name = "Tanveer",RollNo=02,Course= "Java",Fee= 60000,Address="Srinagar"},
+                new StudentDetails(){Name ="Zara",RollNo= 03 , Course = "C#", Fee= 70000, Address="Humhama"},
+                new StudentDetails(){Name = "Aman",RollNo= 04,Course= "C++",Fee= 80000,Address="Srinagar"},
+                new StudentDetails(){Name = "Rohit",RollNo= 05,Course= "JavaScript",Fee= 90000,Address="Budgam"}
+            };
+            return View(std);
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ MyFirstProject/Models/StudentDetails.cs
@@ -0,0 +1,12 @@
+﻿namespace MyFirstProject.Models
+{
+    public class StudentDetails
+    {
+
+        public string Name { get; set; }
+        public int RollNo { get; set; }
+        public string Course { get; set; }
+        public int Fee { get; set; }
+        public string Address { get; set; }
+    }
+}
